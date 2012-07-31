a6882059af2861a8362d37bbefccb7cfd44c6821
jczetta <jczetta@gmail.com>
2012-07-31 14:50:32 -0400

    small changes toward bigger revision

--- lexer_parser.rb
+++ lexer_parser.rb
@@ -32,22 +32,24 @@ def lex_input(input)
   tokens
 end
 
-
-def multdiv_helper(input, newval) 
-  # assumes valid input
+def multdiv_helper(input, newval) # mishandling operator without final number
+  # assumes valid input -- maybe shouldn't
   multdiv = [:times, :div]
   while !input.empty? && multdiv.include?(input[0].type)
     new_op = input.shift
     next_num = input.shift
-    if new_op.type == :times
-      newval *= next_num.value
-    elsif new_op.type == :div
-      newval /= next_num.value
-    end  
+    if next_num && next_num.type == :number # with first check it accepts invalid input with trailing operator, with both it breaks but not my error
+      if new_op.type == :times
+        newval *= next_num.value
+      elsif new_op.type == :div
+        newval /= next_num.value
+      end  
+    end
   end
-  newval
+  input.shift(3) # -- yet with the shift in this line, no fix (see comment below fxn)
+  return newval
 end
-      
+# potential problem: it stops shifting, no longer a stream, breaks when it's wrong the wrong way 
 
 
 def parse_expression(input)
@@ -73,16 +75,18 @@ def parse_expression(input)
         total *= n.value
     elsif op.type == :div
       total /= n.value
-    elsif input[1] 
-      if input[1].type == :number && muldiv.include?(input[0].type)
-        newval = multdiv_helper(input,newval)  
+    # begin peeking (and nesting)
+    elsif input[1]
+      #p input
+      if input[1].type == :number && muldiv.include?(input[0].type) # first check here not correct...
+        newval, input = multdiv_helper(input,newval)  
       elsif input[0].type != :number 
         raise "Syntax error: expected number, got invalid input" 
       end
     else
       newval = n.value
     end
-      
+    
     if op.type == :plus
       total += newval
     elsif op.type == :minus
@@ -99,7 +103,7 @@ end
 #input_string = gets.chomp!  
 #tokens = lex_input(input_string)
 
-tokens = lex_input("    1 -  6   /2 * 3 * 4 / 2 ")
+tokens = lex_input("    1 -  6  /2 + * 3 * 4 /  ")
 #p tokens
 
 puts parse_expression(tokens)
